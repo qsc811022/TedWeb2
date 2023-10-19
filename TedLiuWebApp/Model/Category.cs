2ec6adc362e39112410047116617b6924d044efe
劉敦桐 <zse811022@hotmail.com>
2023-10-20 06:52:47 +0800

    Add Model

---
+++ TedLiuWebApp/Model/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace TedLiuWebApp.Model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
